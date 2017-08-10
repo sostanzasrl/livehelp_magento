c15a050a22c672754cd98483209c4ad9cf698bcd
sostanza <a.samperi@sostanza.it>
2017-08-10 15:14:52 +0200

    Test app

---
+++ app/code/Sostanza/LiveHelp/Block/LiveHelp.php
@@ -0,0 +1,8 @@
+<?php
+    namespace Sostanza\LiveHelp\Block;
+
+    class LiveHelp extends \Magento\Framework\View\Element\Template {
+        public function getLiveHelp () {
+            return 'Hello world!';
+        }
+    }
---
+++ registration.php
@@ -0,0 +1,11 @@
+
+<?php
+
+/**
+ * @author    Tony Samperi
+ * @copyright Copyright (c) 2017 Sostanza (http://www.livehelp.it)
+ * @package   Sostanza_LiveHelp
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE, "Sostanza_LiveHelp", __DIR__
+);
